8a81f7488cab41acb0cdfc53f7e730743361f396
KICreativo <kicreativos@gmail.com>
2018-11-22 10:47:04 +0300

    fix model vendor

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 # call the proprietary setup
-$(call inherit-product-if-exists, vendor/asus/Z00RD/Z00RD-vendor.mk)
+$(call inherit-product-if-exists, vendor/asus/Z00xD/Z00xD-vendor.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
